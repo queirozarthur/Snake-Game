ff82ec4a99f16d7911583dbc1194c2c043f3d7a8
Arthur Queiroz <queirozarthur2109@gmail.com>
2025-09-02 17:11:04 -0300

    Update main.py
    Criação da Def gerar_comida

--- main.py
+++ main.py
@@ -18,12 +18,28 @@ verde = (0, 255, 0)
 
 # parametros cobrinha
 
-tamanho_cobrinha = 10
-velocidade_cobrinha = 15
+tamanho_quadrado = 20
+velocidade_cobra = 15
+
+def gerar_comida():
+    comida_x = round(random.randrange(0, largura - tamanho_quadrado) / 20.0)* 20.0
+    comida_y = round(random.randrange(0, altura - tamanho_quadrado) / 20.0)* 20.0
+    return comida_x, comida_y
 
 
 def rodar_jogo():
     fim_jogo = False
+
+    x = largura / 2
+    y = altura / 2
+
+    velocidade_x = 0
+    velocidade_y = 0 
+
+    tamanho_cobra = 1
+    pixels = []
+
+    comida_x, comida_y = gerarcomida()
 
     while not fim_jogo:
         tela.fill(preta)
